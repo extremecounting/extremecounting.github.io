a9103dcda9c788c93bb770130678b30d93d77355
extremecounting <97205088+extremecounting@users.noreply.github.com>
2023-11-09 07:35:13 -0500

    Delete about.js

--- about.js
+++
@@ -1,7 +0,0 @@
-let btn = document.querySelector('.container');
-btn.addEventListener('mousemove', e => {
-  let x = e.clientX;
-  let y = e.clientY + window.scrollY;
-  btn.style.setProperty('--x', x + 'px');
-  btn.style.setProperty('--y', y + 'px');
-});
