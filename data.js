37c16b684bfa17dde04292d67ad568560f03e2ca
extremecounting <97205088+extremecounting@users.noreply.github.com>
2023-11-27 22:11:41 -0500

    stuff

--- data.js
+++ data.js
@@ -1,14 +1,20 @@
 const itemDatabase = [
     {id:"gd_burger",
     name: "Game Day Burger",
-    price: 13.99,
+    price: 9.99,
     description: "This is a description",
     imgPath: "images/1.png"},
     {id:"onion_rings",
     name: "Onion Rings",
-    price: 8.99,
+    price: 9.99,
     description: "This is a description",
     imgPath: "images/2.png"},
+    {id:"fries",
+    name: "Fries",
+    price: 9.99,
+    description: "This is a description",
+    imgPath: "images/3.png"},
+    {}
 ];
 
 /*
