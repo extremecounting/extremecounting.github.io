73147ee0600d1e90389068a1f86493ea5df7f65c
extremecounting <jakecounting10@gmail.com>
2023-11-28 08:27:29 -0500

    Mega changes :smile:

--- cart.js
+++ cart.js
@@ -1,13 +1,29 @@
 
 document.getElementById('add-burger').addEventListener('click', function() {
+
   if (localStorage.getItem('gd_burger') == null) {
     localStorage.setItem('gd_burger', 1);
   } else {
     localStorage.setItem('gd_burger', +localStorage.getItem('gd_burger') + 1);
   }
-  console.log(localStorage.getItem('burgers'));
 });
 
 document.getElementById('remove-burger').addEventListener('click', function() {
   localStorage.removeItem('gd_burger');
+});
+
+document.getElementById('add-fries').addEventListener('click', function() {
+  if (localStorage.getItem('fries') == null) {
+    localStorage.setItem('fries', 1);
+  } else {
+    localStorage.setItem('fries', +localStorage.getItem('fries') + 1);
+  }
+});
+
+document.getElementById('remove-fries').addEventListener('click', function() {
+  localStorage.removeItem('fries');
+});
+
+document.getElementById('clear-all').addEventListener('click', function() {
+  localStorage.clear();
 });
--- data.js
+++ data.js
@@ -16,19 +16,3 @@ const itemDatabase = [
     imgPath: "images/3.png"},
     {}
 ];
-
-/*
-const gd_burger = {
-    name: "Game Day Burger",
-    price: 13.99,
-    description: "This is a description",
-    imgPath: "images/1.png"
-};
-
-const onion_rings = {
-    name: "Onion Rings",
-    price: 8.99,
-    description: "This is a description",
-    imgPath: "images/2.png"
-};
-*/
