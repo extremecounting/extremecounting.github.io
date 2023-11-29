8472206ccc3e0fb6f70120520a28f5d6a804f16d
ItzOss17 <79410913+ItzOss17@users.noreply.github.com>
2023-11-29 12:44:11 -0500

    A lot of stuff

--- data.js
+++ data.js
@@ -1,19 +1,26 @@
 const itemDatabase = [
+    //burgers
     {id:"gd_burger",
     name: "Game Day Burger",
     price: 9.99,
     description: "This is a description",
     imgPath: "images/1.png"},
+
+    //combos
     {id:"wing_combo1",
     name: "Four Tradional Wings With Fries",
     price: 11.99,
     description: "A combo that combines our signature traditional wings now in a value basket alongside our Game Fries.",
-    imgPath: "wing_combo1.png"},
+    imgPath: "images/wing_combo1.png"},
+
+    //wings
     {id:"6wing",
     name: "Six Tradional Wings",
     price: 9.99,
     description: "Six of our signature traditional wings with a choice of sauce.",
-    imgPath: "6wing.png"},
+    imgPath: "images/6wing.png"},
+
+    //sides and appetizers
     {id:"onion_rings",
     name: "Onion Rings",
     price: 9.99,
@@ -24,5 +31,4 @@ const itemDatabase = [
     price: 9.99,
     description: "This is a description",
     imgPath: "images/3.png"},
-    {}
 ];
