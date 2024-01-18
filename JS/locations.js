38894bafc597411ca6b7a661323ed8823096c903
extremecounting <97205088+extremecounting@users.noreply.github.com>
2024-01-17 22:11:26 -0500

    google maps

--- JS/locations.js
+++ JS/locations.js
@@ -1,18 +1,45 @@
 let map;
 
-function initMap() {
- const position = { lat: -25.344, lng: 131.031 };
- map = new google.maps.Map(document.getElementById("map"), {
-   zoom: 4,
-   center: position,
- });
+async function initMap() {
+  // The location of Uluru
+  //45.745445428957446, -87.07909329557702
+  let positions = [];
+  //Escanaba Location
+  positions.push({ lat: 45.745, lng: -87.079 });
+  //Grand Rapids Location
+  positions.push({lat: 42.978, lng: -85.671});
+  //Chicago Location 41.885914898149274, -87.62769069845365
+  positions.push({lat: 41.886, lng: -87.627});
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  //42.97751978451968, -85.67076822638352
+  // The map, centered at Uluru
+  
+  let lat = 0;
+  let lng = 0;
+  positions.forEach(function(position) {
+    lat += position.lat;
+    lng += position.lng;
+  });
+  lat /= positions.length;
+  lng /= positions.length;
+  let center = {lat:lat, lng: lng};
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 5,
+    center: center,
+    mapId: "DEMO_MAP_ID",
+  });
+  positions.forEach(function(position) {
+    const marker = new AdvancedMarkerElement({
+      map: map,
+      position: position,
+      title: "Restaurant Location",
+    });
+  })
+  
 }
 
-function addPanToMarker(map, markers) {
-    markers.forEach(marker => {
-      marker.addListener('click', event => {
-        const location = { lat: event.latLng.lat(), lng: event.latLng.lng() };
-        map.panTo(location);
-      });
-    });
-}
+initMap();
